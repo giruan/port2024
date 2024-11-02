7a01c1f3a920dd5c18a00d35877613d835ba72a7
unknown <cbk7075@gmail.com>
2024-11-02 17:42:37 +0900

    리액트 헤더완료

--- src/components/Skip.jsx
+++ src/components/Skip.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 
 const Skip = () => {
   return (
-    <div>
-      Skip
+    <div id="skip">
+      <a href='#header'>헤더 영억 바로가기</a>
+      <a href='#intro'>소개 영억 바로가기</a>
+      <a href='#skill'>스킬 영억 바로가기</a>
+      <a href='#site'>사이트 영억 바로가기</a>
+      <a href='#port'>포트폴리오 영억 바로가기</a>
+      <a href='#contact'>연락처 영억 바로가기</a>
+      <a href='#footer'>푸터 영억 바로가기</a>
     </div>
   )
 }
