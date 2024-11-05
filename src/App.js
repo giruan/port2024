fa5018710e1f48c5829261570318b66600c37380
unknown <cbk7075@gmail.com>
2024-11-06 00:41:56 +0900

    1차 완성

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () =>{
       }, []);
     
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
             <Route path="/" element={<HomeView />} />
         </Routes>
